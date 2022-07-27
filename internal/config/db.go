de56f2d1276b9d02cc512371650b3e9919e4eb7f
Thiago Oliveira <thiago.oliveira@sensedia.com>
2022-07-27 18:44:33 -0300

    changing db to postgres

--- internal/config/db.go
+++ internal/config/db.go
@@ -4,14 +4,15 @@ import (
 	"log"
 
 	"github.com/thiiluh/kombibeer/pkg/beers"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(sqlite.Open("beer.db"), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
